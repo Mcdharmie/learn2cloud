aaf28aed02e0b477037f64ddb033ac6b69fccb92
Mcdharmie <damilola.afolayan07@gmail.com>
2025-04-10 21:13:07 +0000

    learning about variables

---
+++ twwn/variable.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+    default = "/home/mcdharmie/learn2cloud/twwn/pets.txt"
+}
+variable "content" {
+    default = "We love pets!"
+}
+variable "prefix" {
+    default = "Mrs"
+}
+variable "separator" {
+    default = "."
+}
+variable "length" {
+    default = "1"
+}
